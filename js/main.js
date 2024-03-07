59785804e1c8b89dbbe845cfb1e4a520a2827303
maddieebeck <102751617+maddieebeck@users.noreply.github.com>
2024-03-07 18:26:29 -0500

    attendence 3/7 sliders

--- js/main.js
+++ js/main.js
@@ -18,15 +18,18 @@ closeBtn.addEventListener('click', function () {
 
 const displayCategory = (category, properties) => {
   const sectionElement = document.createElement('section');
-  sectionElement.classList.add('category');
+  sectionElement.classList.add('slider');
 
   const containerDiv = document.createElement('div');
-  containerDiv.classList.add('container');
+  containerDiv.classList.add('slider__container');
 
-  const sectionTitle = document.createElement('h2');
-  sectionTitle.textContent = category.label.plural;
+  const sliderGrid = document.createElement('div');
+  sliderGrid.classList.add('slider__grid');
 
-  containerDiv.appendChild(sectionTitle);
+  //const sectionTitle = document.createElement('h2');
+  //sectionTitle.textContent = category.label.plural;
+
+  //containerDiv.appendChild(sectionTitle);
 
   //console.log(category.label.singular);
   //1. filter properties
@@ -48,7 +51,7 @@ const displayCategory = (category, properties) => {
   //console.log({ filteredProperties });
   filteredProperties.forEach((property) => {
     const articleElement = document.createElement('article');
-    articleElement.classList.add('property');
+    articleElement.classList.add('slider__item');
 
     let propertyHtml = `
     <h3 class="property--title">${property.name}</h3>
@@ -58,10 +61,11 @@ const displayCategory = (category, properties) => {
 
     articleElement.innerHTML = propertyHtml;
 
-    containerDiv.appendChild(articleElement);
+    sliderGrid.appendChild(articleElement);
   }); // end of forEach
 
   //2. loop and append properties
+  containerDiv.appendChild(sliderGrid);
   sectionElement.appendChild(containerDiv);
   contentDiv.appendChild(sectionElement);
 }; //end of displayCategory
